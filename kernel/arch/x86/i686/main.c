52eed1bca418069862b454783918bfb3112c4983
rgimad <rustem.gimadutdinov@gmail.com>
2021-11-03 22:41:48 +0300

    [ARCH:i686] Add getting physical addresses of kernel start and end

--- kernel/arch/x86/i686/main.c
+++ kernel/arch/x86/i686/main.c
@@ -1,27 +1,33 @@
 #include <kernel/types.h>
+
+#include "../vgatext.h"
 #include <kernel/libk/assert.h>
 #include <kernel/multiboot.h>
 #include <kernel/io/kterm.h>
-#include "../vgatext.h"
+#include <kernel/mm/memlayout.h>
 
 extern void gdt_install(void);
 extern void idt_install(void);
 
 void kmain(struct multiboot* mboot, u32 mboot_magic) {
+    #define PREF "[ARCH i686]: "
     vgatext_init((void *)0xB8000);
     kterm_set_putchar_callback(vgatext_putchar);
     assert(mboot_magic == MULTIBOOT_EAX_MAGIC);
     (void)mboot;
 
-    printk("  Hello from i686 kmain\n");
+    printk(PREF "Hello from kmain\n");
 
     gdt_install();
-    printk("    GDT installed\n");
+    printk(PREF "GDT installed\n");
 
     idt_install();
-    printk("    IDT installed\n");
+    printk(PREF "IDT installed\n");
+
+    printk(PREF "kernel_phys_start = %x\n" PREF "kernel_phys_end = %x\n", KERNEL_START_PHYS, KERNEL_END_PHYS);
 
     while (1) {}
 
     // generic_main();
+    #undef PREF
 }
---
+++ kernel/mm/memlayout.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdint.h>
+
+#define PAGE_SIZE 4096
+
+#define KERNEL_START_PHYS ((uintptr_t)&kernel_phys_start)
+#define KERNEL_END_PHYS ((uintptr_t)&kernel_phys_end)
+#define KERNEL_SIZE (KERNEL_END_PHYS - KERNEL_START_PHYS)
+
+#define ALIGN_BLOCK(addr) (addr) - ((addr) % PHYS_BLOCK_SIZE);
+
+extern uintptr_t kernel_phys_start;
+extern uintptr_t kernel_phys_end;
