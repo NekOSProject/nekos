3443c848337cc09358683f56a1f54ba7980b6e38
rgimad <33692565+rgimad@users.noreply.github.com>
2021-11-05 20:09:10 +0300

    [MM] Fix ALIGN_BLOCK macro

--- kernel/mm/memlayout.h
+++ kernel/mm/memlayout.h
@@ -8,7 +8,7 @@
 #define KERNEL_END_PHYS ((uintptr_t)&kernel_phys_end)
 #define KERNEL_SIZE (KERNEL_END_PHYS - KERNEL_START_PHYS)
 
-#define ALIGN_BLOCK(addr) (addr) - ((addr) % PHYS_BLOCK_SIZE);
+#define ALIGN_BLOCK(addr) ((addr) - ((addr) % PHYS_BLOCK_SIZE))
 
 extern uintptr_t kernel_phys_start;
 extern uintptr_t kernel_phys_end;
